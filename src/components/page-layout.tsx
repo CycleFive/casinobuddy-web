cb8c0fba4ea5eaa81e52ef0c329bc037e3d10f50
mpalmr <matthew.palmer1@mohawkcollege.ca>
2024-10-15 08:52:06 -0400

    add login button and user button to layout, as well as fix height of <main> element to take up as much of the page as it can

--- src/components/page-layout.tsx
+++ src/components/page-layout.tsx
@@ -1,7 +1,28 @@
 import React, { FC, ReactNode } from 'react';
 import styled from 'styled-components';
 import { Link } from 'react-router-dom';
+import {
+  SignedIn,
+  SignedOut,
+  SignInButton,
+  UserButton,
+} from '@clerk/clerk-react';
 import { Navbar, Nav, Container } from 'react-bootstrap';
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+
+  > main {
+    flex-grow: 1;
+  }
+`;
+
+const LeftHeader = styled.div`
+  display: flex;
+  align-items: center;
+`;
 
 const MainNav = styled(Nav)`
   display: flex;
@@ -26,25 +47,36 @@ interface Props {
 
 const PageLayout: FC<Props> = function PageLayout({ children }) {
   return (
-    <>
+    <Wrapper>
       <Navbar as="header">
-        <Container fluid>
-          <Navbar.Brand as={Link} to="/">Casinobuddy</Navbar.Brand>
-          <Navbar.Toggle aria-controls="main-navbar-nav" />
-          <Navbar.Collapse id="main-navbar-nav">
-            <MainNav as="nav">
-              <ul>
-                <Nav.Item as="li">
-                  <Nav.Link as={Link} to="/dashboard">Dashboard</Nav.Link>
-                </Nav.Item>
-              </ul>
-            </MainNav>
-          </Navbar.Collapse>
+        <Container className="justify-content-between" fluid>
+          <LeftHeader>
+            <Navbar.Brand as={Link} to="/">Casinobuddy</Navbar.Brand>
+            <Navbar.Toggle aria-controls="main-navbar-nav" />
+            <Navbar.Collapse id="main-navbar-nav">
+              <MainNav as="nav">
+                <ul>
+                  <Nav.Item as="li">
+                    <Nav.Link as={Link} to="/dashboard">Dashboard</Nav.Link>
+                  </Nav.Item>
+                </ul>
+              </MainNav>
+            </Navbar.Collapse>
+          </LeftHeader>
+
+          <div>
+            <SignedOut>
+              <SignInButton />
+            </SignedOut>
+            <SignedIn>
+              <UserButton />
+            </SignedIn>
+          </div>
         </Container>
       </Navbar>
 
       <main>{children}</main>
-    </>
+    </Wrapper>
   );
 };
 
